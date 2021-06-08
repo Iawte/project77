b6beada3b8f1b87bc19b71e77fb8d815be751787
Iawte <79056849+Iawte@users.noreply.github.com>
2021-06-08 18:23:48 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log("This is my first project in console");
+var firstName="Ishaan";
+var lastName="Awte";
+var name=firstName.concat(lastName);
+var num1=7;
+var num2=8;
+console.log(firstName);
+console.log(lastName);
+console.log(name);
+console.log(num1+num2);
+console.log(num1-num2);
+console.log(num1*num2);
+console.log(num1/num2);
+
+
